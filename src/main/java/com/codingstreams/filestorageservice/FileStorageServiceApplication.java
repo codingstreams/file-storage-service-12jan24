ecdaf2d32a3e8f2a71aa589e2f2c53180015011e
Coding Streams <akxhdev@outlook.com>
2024-01-12 10:01:50 +0530

    Initial Commit

---
+++ src/main/java/com/codingstreams/filestorageservice/FileStorageServiceApplication.java
@@ -0,0 +1,14 @@
+package com.codingstreams.filestorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+public class FileStorageServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileStorageServiceApplication.class, args);
+  }
+
+}
